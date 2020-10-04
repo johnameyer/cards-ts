f6ec71302b0a01d1c10c703cf52fa407b869f7e8
John (Jack) Meyer <jack10042@gmail.com>
2020-10-04 01:05:05 -0400

    Reapply code that disappeared during lerna fiasco

--- packages/can-i-have-that/src/index.ts
+++ packages/can-i-have-that/src/index.ts
@@ -1,4 +1,6 @@
 export { GameStateIterator } from './game-state-iterator';
 export { defaultParams } from './game-params';
 export { LocalMaximumHandler as DefaultBotHandler } from './handlers/local-maximum-handler';
+export { ResponseValidator } from './response-validator';
+export { StateTransformer } from './state-transformer';
 export { IntermediaryHandler } from './handlers/intermediary-handler';
--- packages/hearts/src/index.ts
+++ packages/hearts/src/index.ts
@@ -1,4 +1,6 @@
 export { GameStateIterator } from './game-state-iterator';
 export { defaultParams } from './game-params';
 export { HeuristicHandler as DefaultBotHandler } from './handlers/heuristic-handler';
+export { ResponseValidator } from './response-validator';
+export { StateTransformer } from './state-transformer';
 export { IntermediaryHandler } from './handlers/intermediary-handler';
