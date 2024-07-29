630015b0827f93a95ebe7d15c14be5be82426955
John Meyer <jack10042@gmail.com>
2024-07-29 00:21:46 -0400

    Change event handler signature for easier reuse of common response types (#128)

---
+++ .changeset/fair-otters-clap.md
@@ -0,0 +1,9 @@
+---
+"@cards-ts/can-i-have-that": minor
+"@cards-ts/euchre": minor
+"@cards-ts/hearts": minor
+"@cards-ts/core": minor
+"@cards-ts/war": minor
+---
+
+Change signature of event handler definition for future ease of reusing standard messages and components
--- packages/war/src/event-handler.ts
+++ packages/war/src/event-handler.ts
@@ -3,10 +3,7 @@ import { ResponseMessage } from './messages/response-message.js';
 import { EventHandler, buildEventHandler } from '@cards-ts/core';
 
 export const eventHandler = buildEventHandler<Controllers, ResponseMessage>({
-    merge: {
-        'flip-response': EventHandler.removeWaiting('waiting'),
-    },
-    validateEvent: {
-        'flip-response': (_controllers, _sourceHandler, event) => event,
+    'flip-response': {
+        merge: EventHandler.removeWaiting('waiting'),
     },
 });
